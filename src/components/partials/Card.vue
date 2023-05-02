e5b3d3b2c3c0c90d4f10687ca83d65c7ff1901cb
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-05-02 17:35:37 +0200

    [fix] card title not breaking

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -11,15 +11,15 @@ export default {
 </script>
 
 <template>
-  <div class="mb-card ">
+  <div class="mb-card mb-3">
     <!-- <img :src="cardImage" :alt="cardName"> -->
     <img src="https://images.ygoprodeck.com/images/cards/50371210.jpg" alt="">
-    <div class="text">
-      <span>Beacon of white</span>
+    <div class="mb-text text-center">
+      <p>Beacon of whitedddsssssaaaaaaaaa</p>
       <!-- <span>{{ cardName }}</span> -->
     </div>
-    <div class="archetype">
-      <span>blue eyes</span>
+    <div class="archetype text-center">
+      <span class="my-auto">blue eyes</span>
       <!-- <span>{{ cardArchetype }}</span> -->
     </div>
   </div>
@@ -28,9 +28,30 @@ export default {
 <style lang="scss" scoped>
 .mb-card{
   width: 18%;
+  
   img{
     height: 250px;
     width: 100%;
+  }
+  .mb-text{
+    background-color: #d48f38;
+    height: 100px;
+    padding: 10px;
+    p{
+      word-break: break-all;
+      color: white;
+    }
+  }
+  .archetype{
+    height: 25px;
+    background-color: #d48f38;
+  }
+
+  .mb-text,
+  .archetype{
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 }
 </style>
